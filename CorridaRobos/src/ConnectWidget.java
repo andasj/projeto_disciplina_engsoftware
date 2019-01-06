0c2d2acf550a8915f2dfc71fd350a548e31dc1d7
rebeca53 <rebeca.n.rod@gmail.com>
2019-01-06 16:32:45 -0400

    Primeiro commit do projeto, parte da interface

---
+++ CorridaRobos/src/ConnectWidget.java
@@ -0,0 +1,120 @@
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JTextField;
+
+/**
+ * This widget provides a graphical user interface to connect to Robotino.
+ */
+public class ConnectWidget extends JComponent
+{
+	protected final Robot robot;
+	
+	protected final JTextField textFieldAddress;
+	protected final JButton buttonConnect;
+	
+	public ConnectWidget(Robot robot)
+	{
+		this.robot = robot;
+		
+		buttonConnect = new JButton("Connect");
+		textFieldAddress = new JTextField("127.0.0.1:12080", 12);
+		
+		setLayout( new FlowLayout() );
+		add(textFieldAddress);
+		add(buttonConnect);
+		
+		setMinimumSize( new Dimension(100, 35) );
+		setPreferredSize( new Dimension(100, 35) );
+		setMaximumSize( new Dimension(Short.MAX_VALUE, 35) );
+		
+		buttonConnect.addActionListener( new ConnectButtonActionListener() );
+		robot.addListener( new RobotListenerImpl() );
+	}
+	
+	private class ConnectButtonActionListener implements ActionListener
+	{
+		@Override
+		public void actionPerformed(ActionEvent e)
+		{
+			if(!robot.isConnected())
+			{
+				System.out.println( "Connecting to " + textFieldAddress.getText() + " ...");
+				robot.connect(textFieldAddress.getText(), false);
+			}
+			else
+			{
+				System.out.println( "Disconnecting...");
+				robot.disconnect();
+			}
+			
+			buttonConnect.setEnabled( false );
+		}	
+	}
+	
+	private class RobotListenerImpl implements RobotListener
+	{
+		double lastX=0, lastY=0,xRef=0,yRef=0;
+		boolean firstReading = true;
+		
+		@Override
+		public void onConnected()
+		{
+			EventQueue.invokeLater( new Runnable()
+			{
+				@Override
+				public void run()
+				{
+					buttonConnect.setText( "Disconnect" );
+					buttonConnect.setEnabled( true );					
+				}
+			});
+		}
+
+		@Override
+		public void onDisconnected()
+		{
+			EventQueue.invokeLater( new Runnable()
+			{
+				@Override
+				public void run()
+				{
+					buttonConnect.setText( "Connect" );
+					buttonConnect.setEnabled( true );				
+				}
+			});
+		}
+
+		@Override
+		public void onError(String error)
+		{
+			if(!robot.isConnected())
+				onDisconnected();			
+		}
+
+		@Override
+		public void onImageReceived(Image img)
+		{
+		}
+
+		@Override
+		public void onOdometryReceived(double x, double y, double phi) {
+			if(firstReading){
+				xRef = x;
+				yRef = y;
+				firstReading = false;
+			}
+			
+			if((lastX != x) && (lastY != y)){
+				System.out.println("odometry,"+(x-xRef)+","+(y-yRef)+","+phi);
+			}
+			lastX = x;
+			lastY = y;
+		}
+	}
+}
---
+++ CorridaRobos/src/MapWidget.java
@@ -0,0 +1,176 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Shape;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import javax.swing.JComponent;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.axis.NumberTickUnit;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.util.ShapeUtilities;
+
+
+
+//TODO: use phi to tell direction it is moving and to plot correctly
+//TODO: use a shape that indicates the direction, different color
+//TODO: covering the same area, will erase the path
+//TODO: if it is outside the area, pop up it has failed
+//TODO: store the path
+//TODO: enable learning tour, and knowledge tour options
+
+public class MapWidget extends JComponent {
+	protected JFreeChart map;
+	public XYSeries lineData, series;
+	public XYSeriesCollection dataset;
+	Shape goal = ShapeUtilities.createDiagonalCross(3, 1);
+	final double size = 10.0;
+	double xRef=0, yRef = 0;
+	
+	public MapWidget() {
+		dataset = new XYSeriesCollection();
+
+		lineData = new XYSeries("Line Plot");
+		dataset.addSeries(lineData);
+
+//		// scatter plot
+		XYSeries series = new XYSeries("");
+		series.add(size, size);
+		dataset.addSeries(series);
+
+		map = ChartFactory.createXYLineChart("Robot path", "x (m)", "y (m)", dataset);
+		
+		XYPlot plot = (XYPlot) map.getPlot();
+		NumberAxis domain = (NumberAxis) plot.getDomainAxis();
+        domain.setRange(-size, size);
+        domain.setTickUnit(new NumberTickUnit(size/10));
+        
+        domain.setVerticalTickLabels(true);
+        NumberAxis range = (NumberAxis) plot.getRangeAxis();
+        range.setRange(-size, size);
+        range.setTickUnit(new NumberTickUnit(size/5));
+		
+		XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
+
+		// "0" is the line plot
+		renderer.setSeriesLinesVisible(0, false);
+		renderer.setSeriesShapesVisible(0, true);
+
+		// "1" is the scatter plot
+		renderer.setSeriesLinesVisible(1, false);
+		renderer.setSeriesShapesVisible(1, true);
+		renderer.setSeriesShape(1, goal);
+		
+
+		plot.setRenderer(renderer);
+
+		setLayout(new BorderLayout());
+		add(new ChartPanel(map), BorderLayout.CENTER);
+
+		setMinimumSize(new Dimension(100, 30));
+		setPreferredSize(new Dimension(800, 200));
+		setMaximumSize(new Dimension(Short.MAX_VALUE, Short.MAX_VALUE));
+
+		PrintStream stdOut = new PrintStream(new JFreeChartOutputStream());
+		System.setOut(stdOut);
+		//System.setErr(stdOut);
+
+	}
+
+	private class JFreeChartOutputStream extends OutputStream {
+		private final StringBuffer buffer = new StringBuffer();
+		private double x = 0, y = 0;
+		Boolean toUpdate = new Boolean(false);
+		XYSeries path = new XYSeries("Path");
+
+		@Override
+		public void write(int b) throws IOException {
+			buffer.append((char) b);
+			flush();
+		}
+
+		@Override
+		public void write(byte[] b) throws IOException {
+			write(b, 0, b.length);
+			flush();
+		}
+
+		@Override
+		public void write(byte[] b, int off, int len) throws IOException {
+			if (b == null)
+				throw new NullPointerException();
+			else if ((off < 0) || (off > b.length) || (len < 0) || ((off + len) > b.length) || ((off + len) < 0))
+				throw new IndexOutOfBoundsException();
+			else if (len == 0)
+				return;
+
+			for (int i = 0; i < len; i++)
+				buffer.append((char) b[off + i]);
+
+			flush();
+		}
+
+		@Override
+		public void flush() throws IOException {
+			final String str;
+
+			synchronized (buffer) {
+				str = buffer.toString();
+				buffer.delete(0, buffer.length());
+
+				String coordinates[] = str.split(",");
+				if (coordinates[0].equals("odometry")) {
+					double xNow = (double) Double.parseDouble(coordinates[1]);
+					double yNow = (double) Double.parseDouble(coordinates[2]);
+					double signal = Math.signum(Double.parseDouble(coordinates[3]));
+					
+					// if it is new information
+					if ((x != xNow) && (y != yNow)) {
+						try{
+							path.add(-signal*yNow, signal*xNow);
+						}
+						catch(Exception e){
+							System.err.println(e.getMessage());
+							
+							path = new XYSeries("Path");
+							
+						}
+					}
+
+					x = xNow;
+					y = yNow;
+					toUpdate = true;
+
+				} 
+
+			}
+
+			EventQueue.invokeLater(new Runnable() {
+				@Override
+				public void run() {
+					if (toUpdate) {
+						// update chart
+						dataset.removeAllSeries();
+						dataset.addSeries(path);
+//						series.add(2, 2);
+//						dataset.addSeries(series);
+
+						toUpdate = false;
+						// textArea.append(str);
+					}
+					
+				}
+			});
+		}
+	}
+
+}
